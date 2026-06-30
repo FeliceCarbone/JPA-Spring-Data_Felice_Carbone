ad81c70e499144fab224b833887609ca1e485594
Felice Carbone <felicecarb0013@gmail.com>
2026-06-30 13:58:27 +0200

    aggiunte modifiche

---
+++ src/main/java/it/aulab/progetto_blog/controllers/AuthController.java
@@ -0,0 +1,19 @@
+package it.aulab.progetto_blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class AuthController {
+    @GetMapping("login")
+    public String login(Model model) {
+        model.addAttribute("title", "Login");
+        return "login";
+    }
+    
+}
